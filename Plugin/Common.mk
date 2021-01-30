4dbc1f0504a101672e0bbcbc36908dbfb362b029
Keijiro Takahashi <keijiro@gmail.com>
2021-01-30 22:14:08 +0900

    Refactor Makefile

--- Plugin/Common.mk
+++ Plugin/Common.mk
@@ -1,44 +1,79 @@
-TARGET = TestPlugin
+#
+# File listings
+#
 
+TARGET = TestPlugin
 SRCS = Test.cpp
+OUT_DIR = ../Assets
+
+#
+# Intermediate/output files
+#
+
 OBJS = $(SRCS:.cpp=.o)
 
-CXXFLAGS = -O2 -Wall -std=c++11
-LDFLAGS = -shared
-
-ifeq ($(PLATFORM), Windows)
-    BIN_PREFIX = x86_64-w64-mingw32
-    OUTPUT = $(TARGET).dll
+ifeq ($(OUTPUT_TYPE), so)
+  OUTPUT = lib$(TARGET).$(OUTPUT_TYPE)
 else
-    BIN_PREFIX =
-    CXXFLAGS += -fPIC
-    LDFLAGS += -rdynamic -fPIC
-    ifeq ($(PLATFORM), MacOS)
-        CXXFLAGS += -target x86_64-apple-macos10.12
-        LDFLAGS += -target x86_64-apple-macos10.12
-        OUTPUT = $(TARGET).bundle
-        NOSTRIP = true
-    else
-        OUTPUT = lib$(TARGET).so
-    endif
+  OUTPUT = $(TARGET).$(OUTPUT_TYPE)
 endif
 
-CXX = $(BIN_PREFIX)-c++
-STRIP = $(BIN_PREFIX)-strip
+#
+# Compiler/linker options
+#
+
+CXXFLAGS += -O2 -Wall -std=c++11
+
+LDFLAGS += -shared
+
+ifeq ($(OUTPUT_TYPE), dll)
+else ifeq ($(OUTPUT_TYPE), a)
+else
+  CXXFLAGS += -fPIC
+  LDFLAGS += -rdynamic -fPIC
+endif
+
+#
+# Toolchain
+#
+
+ifdef TOOLCHAIN
+  BIN_PREFIX=$(TOOLCHAIN)-
+endif
+
+AR = $(BIN_PREFIX)ar
+CXX = $(BIN_PREFIX)c++
+STRIP = $(BIN_PREFIX)strip
+
+# Don't try to strip .bundle files.
+ifeq ($(OUTPUT_TYPE), bundle)
+  STRIP = true
+endif
+
+#
+# Building rules
+#
 
 all: $(OUTPUT)
 
 copy: $(OUTPUT)
-ifndef NOSTRIP
 	$(STRIP) $(OUTPUT)
-endif
-	cp -f $(OUTPUT) ../Assets/$(OUTPUT)
+	cp -f $(OUTPUT) $(OUT_DIR)/$(OUTPUT)
 
 clean:
 	rm -f $(OUTPUT) $(OBJS)
 
-$(OUTPUT): $(OBJS)
-	$(CXX) $(LDFLAGS) -o $(OUTPUT) $(OBJS)
+$(TARGET).dll: $(OBJS)
+	$(CXX) $(LDFLAGS) -o $@ $<
+
+$(TARGET).bundle: $(OBJS)
+	$(CXX) $(LDFLAGS) -o $@ $<
+
+lib$(TARGET).so: $(OBJS)
+	$(CXX) $(LDFLAGS) -o $@ $<
+
+$(TARGET).a : $(OBJS)
+	$(AR) -crv $@ $<
 
 .cpp.o:
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
